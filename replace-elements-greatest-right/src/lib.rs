df3d3aecc3ce9041187906899ce9cca742f6a9e8
danandreicarp <carpdan@pm.me>
2022-09-13 08:12:00 +0300

    replace elements with greatest right fix

--- replace-elements-greatest-right/src/lib.rs
+++ replace-elements-greatest-right/src/lib.rs
@@ -1,7 +1,7 @@
-pub fn replace_elements(arr: &mut Vec<i32>) -> Vec<i32> {
+pub fn replace_elements(mut arr: Vec<i32>) -> Vec<i32> {
     if arr.len() == 1 {
         arr[0] = -1;
-        return arr.to_owned();
+        return arr;
     }
 
     let mut i = arr.len() - 1;
@@ -19,7 +19,7 @@ pub fn replace_elements(arr: &mut Vec<i32>) -> Vec<i32> {
 
     arr[i] = greatest;
 
-    return arr.to_owned();
+    return arr;
 }
 
 #[cfg(test)]
@@ -28,15 +28,22 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let mut arr = vec![17, 18, 5, 4, 6, 1];
-        let result = replace_elements(&mut arr);
+        let arr = vec![17, 18, 5, 4, 6, 1];
+        let result = replace_elements(arr);
         assert_eq!(result, vec![18, 6, 6, 6, 1, -1]);
     }
 
     #[test]
     fn test_2() {
-        let mut arr = vec![400];
-        let result = replace_elements(&mut arr);
+        let arr = vec![400];
+        let result = replace_elements(arr);
         assert_eq!(result, vec![-1]);
+    }
+
+    #[test]
+    fn test_3() {
+        let arr = vec![57010, 40840, 69871, 14425, 70605];
+        let result = replace_elements(arr);
+        assert_eq!(result, vec![70605, 70605, 70605, 70605, -1]);
     }
 }
