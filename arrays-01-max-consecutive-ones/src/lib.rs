4a21a49b6f46406f0c41499180ebe1067a325169
Dan Carp <carpdan@amazon.com>
2024-07-06 15:48:46 +0300

    update find_max_consecutive_ones

--- arrays-01-max-consecutive-ones/src/lib.rs
+++ arrays-01-max-consecutive-ones/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn find_max_consecutive_ones(nums: &[i32]) -> i32 {
+pub fn find_max_consecutive_ones_original(nums: &[i32]) -> i32 {
     let mut count = 0;
     let mut max = 0;
 
@@ -6,12 +6,41 @@ pub fn find_max_consecutive_ones(nums: &[i32]) -> i32 {
         if *i == 1 {
             count = count + 1;
         } else {
-            max = count;
+            max = max.max(count);
             count = 0;
         }
     }
 
     max.max(count)
+}
+
+pub fn find_max_consecutive_ones(nums: &[i32]) -> i32 {
+    let mut is_in = false;
+    let mut start = 0;
+    let mut max = 0;
+
+    for i in 0..nums.len() {
+        if nums[i] == 0 {
+            if is_in {
+                is_in = false;
+                let length = i - start;
+                if length > max {
+                    max = length;
+                }
+            }
+        } else {
+            if is_in == false {
+                is_in = true;
+                start = i;
+            }
+        }
+    }
+
+    if is_in {
+        max.max(nums.len() - start) as i32
+    } else {
+        max as i32
+    }
 }
 
 #[cfg(test)]
@@ -46,5 +75,11 @@ mod tests {
     fn test_5() {
         let result = find_max_consecutive_ones(&[1, 1, 1, 1]);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_6() {
+        let result = find_max_consecutive_ones(&[1,1,1,0,1,1,0]);
+        assert_eq!(result, 3);
     }
 }
